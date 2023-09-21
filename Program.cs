83fd2dfa1e8f3b75253f9927ae5fdf8d0b8e98aa
abdulazeez shittu <shittuabdulazeezas@gmail.com>
2023-09-21 10:06:19 +0100

    my first dice game

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿//Build Dice Game
 Console.WriteLine("====Simple Dice Game====\nType 'Start' or 'S' to roll the die:");
-
 string startGame = Console.ReadLine()!;
 
-if (!(startGame.Equals("Start",StringComparison.OrdinalIgnoreCase) || startGame.Equals("S",StringComparison.OrdinalIgnoreCase)))
+while (!(startGame.Equals("Start",StringComparison.OrdinalIgnoreCase) || startGame.Equals("S",StringComparison.OrdinalIgnoreCase)))
 {
-    Console.WriteLine("Enter 'Start' or 'S' to start the game properly.");
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("Invalid key word! ");
+    Console.ResetColor(); 
+    Console.Write("Type 'Start' or 'S' to roll the die:");
+    startGame = Console.ReadLine()!;
     return;
 }
-
+ 
 Random random = new();
 int diceOne = random.Next(1, 7);
 int diceTwo = random.Next(1, 7);
@@ -36,37 +39,31 @@ if (totalPoint >= 10 && totalPoint <=13)
 {
     Console.WriteLine("Congratulations you win a ballon");
 }
-
 //Now for the exercise book
 else if (totalPoint >= 14 && totalPoint <= 16)
 {
     Console.WriteLine("Congratulations you win an exercise book");
 }
-
 //Now for the power bank
 else if (totalPoint == 17)
 {
     Console.WriteLine("Congratulations you win a 5000MAH Power Bank");
 }
-
 //Now for the silverbird gallery
 else if (totalPoint >= 18 && totalPoint <= 20)
 {
     Console.WriteLine("Congratulations you win a ticket to silverbird gallery");
 }
-
 //Now for lucky shot
 else if (totalPoint == 21)
 {
     Console.WriteLine("Congratulations you win a lucky shot, you get samsung A24");
 }
-
 //Now for all items
 else if (totalPoint >= 22 && totalPoint <= 24)
 {
     Console.WriteLine("Congratulations you win all items");
 }
-
 else
 {
     Console.WriteLine("Sorry try again later!");
